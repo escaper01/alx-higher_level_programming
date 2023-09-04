b6dac2bafb2b894eaceaca392a8523871bccbc23
escaper <assaoui54@gmail.com>
2023-09-05 00:19:48 +0100

    cycle list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,2 +1,30 @@
-#!/usr/bin/python3
-import this
+#include <stdio.h>
+#include "lists.h"
+/**
+* check_cycle - checks if a singly linked list is cycled
+* @list: singly linked list.
+* Return: 0 or 1
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head;
+	listint_t *tail;
+
+	if (list == NULL)
+		return (0);
+
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
+	{
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
+			return (1);
+
+	}
+	return (0);
+}
