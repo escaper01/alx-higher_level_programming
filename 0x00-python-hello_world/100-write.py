d7cd60e92ba2ea45af924682272807334afe0a77
escaper <assaoui54@gmail.com>
2023-09-05 11:03:16 +0100

    optional taks 3

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 from sys import stderr, exit
-stderr.buffer.write(b'and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+
+buff = stderr.buffer
+buff.write(b'and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 exit(1)
