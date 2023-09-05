125a9da60cc32075d78cb04f5ab367a64ce425ee
escaper <assaoui54@gmail.com>
2023-09-05 15:09:38 +0100

    task 12

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 def pow(a, b):
-	res = 1
-	base = 1
-	num = 0
+    res = 1
+    base = 1
+    num = 0
 
-	if b < 0:
-		num = b
-		b = (-1) * b
+    if b < 0:
+        num = b
+        b = (-1) * b
 
-	for i in range(b):
-		res *= a
-		base = res * res
+    for i in range(b):
+        res *= a
+        base = res * res
 
-	if num  < 0:
-		res /= base
-	return res
+    if num < 0:
+        res /= base
+    return res
