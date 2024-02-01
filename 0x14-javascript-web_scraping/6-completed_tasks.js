3c7ded1bce89a99a80a5fd6cb517d6cd35d488d1
escaper <assaoui54@gmail.com>
2024-02-01 02:18:58 +0100

    fix: another fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,13 +2,13 @@
 // A script that computes the number of tasks completed by user id.
 
 const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
+const reqURL = args[2];
+const request = require('request');
 request(reqURL, function (error, response, body) {
   if (error) {
     console.log('error:', error);
   } else {
-    let todos = JSON.parse(body);
+    const todos = JSON.parse(body);
     let dash = {};
     for (let i = 0; i < todos.length; i++) {
       let status = (todos[i]['completed']);
